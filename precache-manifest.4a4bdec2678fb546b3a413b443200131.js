21325fa9f280d27575e8969f908a55d5617a763c
AndersOestergaard <Anders_1502@hotmail.com>
2020-06-21 19:26:20 +0100

    Updates

---
+++ precache-manifest.4a4bdec2678fb546b3a413b443200131.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d6da8ecba5344eb825e78939401cef",
+    "url": "/react-dashboard/index.html"
+  },
+  {
+    "revision": "b3beae45833b01842e50",
+    "url": "/react-dashboard/static/css/2.01dd2b47.chunk.css"
+  },
+  {
+    "revision": "169afd61dae2bb95a13d",
+    "url": "/react-dashboard/static/css/main.ea670a58.chunk.css"
+  },
+  {
+    "revision": "b3beae45833b01842e50",
+    "url": "/react-dashboard/static/js/2.f6e5a255.chunk.js"
+  },
+  {
+    "revision": "81b6e0f34fc5a80a94fad82949fb96e7",
+    "url": "/react-dashboard/static/js/2.f6e5a255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "169afd61dae2bb95a13d",
+    "url": "/react-dashboard/static/js/main.d5005216.chunk.js"
+  },
+  {
+    "revision": "836df333bf0fac186111",
+    "url": "/react-dashboard/static/js/runtime-main.ab9e6ad5.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/react-dashboard/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/react-dashboard/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/react-dashboard/static/media/revicons.2feb69cc.eot"
+  }
+]);
